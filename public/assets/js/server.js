f48635e1e2577a13a6e79c2708b99027415ad59b
kshwi <kshwiss@gmail.com>
2023-04-22 14:17:55 -0500

    moved the listen to where it should be in server.js

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -9,6 +9,10 @@ app.get("*", (req, res) => res.sendFile(path.join(__dirname, "/index.html")));
 
 app.get("/api/notes", (req, res) => res.JSON(path.join(__dirname, "/db.json")));
 
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
+
 app.post("/api/notes", (req, res) => {
     const {title, content } = req.body;
     const db = JSON.parse(data);
@@ -24,11 +28,9 @@ app.post("/api/notes", (req, res) => {
         app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
     })
-
-    app.listen(PORT, () => {
-        console.log(`Server listening on port ${PORT}`);
     });
-
+ 
+    
     // Method to generate unique ID 
   //generateUniqueID() {
    // return crypto.randomBytes(8).toString('hex')
