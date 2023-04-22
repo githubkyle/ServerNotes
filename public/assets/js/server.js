8c86fb5f9b5e1fa73348ca042a5ab0ae132f48cd
kshwi <kshwiss@gmail.com>
2023-04-22 13:38:24 -0500

    added some get and post request code

---
+++ public/assets/js/server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const fs = require('fs');
+const PORT = 3009;
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/notes.html"))
+);
+app.get("*", (req, res) => res.sendFile(path.join(__dirname, "/index.html")));
+
+app.get("/api/notes", (req, res) => res.JSON(path.join(__dirname, "/db.json")));
+
+app.post("/api/notes", (req, res) => {
+    const {title, content } = req.body;
+    const db = JSON.parse(data);
+    const newNote = { title, content };
+    db.notes.push(newNote);
+    fs.writeFile('db.json', JSON.stringify(db), (err) => {
+        if(err) {
+            console.error(err);
+            return res.status(500).send('Error adding that note');
+        }
+
+        res.status(201).json(newNote);
+        app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+    })
+
+    app.listen(PORT, () => {
+        console.log(`Server listening on port ${PORT}`);
+    });
+
+    // Method to generate unique ID 
+  //generateUniqueID() {
+   // return crypto.randomBytes(8).toString('hex')
+ // }
+//}
