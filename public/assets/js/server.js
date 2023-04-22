15364144463f3d774fce255d4b46b809aef1ec81
kshwi <kshwiss@gmail.com>
2023-04-22 15:00:30 -0500

    minor troubleshooting in server.js

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const fs = require('fs');
+const fs = require("fs");
 const PORT = 3009;
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/notes.html"))
@@ -9,28 +9,26 @@ app.get("*", (req, res) => res.sendFile(path.join(__dirname, "/index.html")));
 
 app.get("/api/notes", (req, res) => res.JSON(path.join(__dirname, "/db.json")));
 
-
-
 app.post("/api/notes", (req, res) => {
-    const {title, content } = req.body;
-    const db = JSON.parse(data);
-    const newNote = { title, content };
-    db.notes.push(newNote);
-    fs.writeFile('db.json', JSON.stringify(db), (err) => {
-        if(err) {
-            console.error(err);
-            return res.status(500).send('Error adding that note');
-        }
-    });
-    res.status(201).json(newNote);
-}
+  const { title, content } = req.body;
+  const db = JSON.parse(data);
+  const newNote = { title, content };
+  db.notes.push(newNote);
+  fs.writeFile("db.json", JSON.stringify(db), err => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send("Error adding that note");
+    }
+  });
+  res.status(201).json(newNote);
+});
 
 app.listen(PORT, () => {
-     console.log(`Server listening on port ${PORT}`);
- });
-    
-    // Method to generate unique ID 
-  //generateUniqueID() {
-   // return crypto.randomBytes(8).toString('hex')
- // }
+  console.log(`Server listening on port ${PORT}`);
+});
+
+// Method to generate unique ID
+//generateUniqueID() {
+// return crypto.randomBytes(8).toString('hex')
+// }
 //}
