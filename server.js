ce6f7d4dd42d794fa0c410a646ffe7619a3643b2
kshwi <kshwiss@gmail.com>
2023-04-23 15:48:12 -0500

    commented out the path that was breaking it

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = 3009;
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/notes.html"))
 );
-app.get("*", (req, res) => res.sendFile(path.join(__dirname, "/index.html")));
+// app.get("*", (req, res) => res.sendFile(path.join(__dirname, "/index.html")));
 
 app.get("/api/notes", (req, res) => res.JSON(path.join(__dirname, "/db.json")));
 
