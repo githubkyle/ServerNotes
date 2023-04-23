ef01718728ed95a5cdce01a656cb49d3f996d92d
kshwi <kshwiss@gmail.com>
2023-04-23 14:42:29 -0500

    added the random id generator to the post request logic

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const fs = require("fs");
 const PORT = 3009;
+const { v4: uuidv4 } = require("uuid");
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/notes.html"))
 );
@@ -10,25 +11,25 @@ app.get("*", (req, res) => res.sendFile(path.join(__dirname, "/index.html")));
 app.get("/api/notes", (req, res) => res.JSON(path.join(__dirname, "/db.json")));
 
 app.post("/api/notes", (req, res) => {
-  const { title, content } = req.body;
+  const newNote = req.body;
+  const newNoteWithId = { ...newNote, id: uuidv4() };
+
+  res.json(newNoteWithId);
+
   const db = JSON.parse(data);
-  const newNote = { title, content };
-  db.notes.push(newNote);
+
+  db.notes.push(newNoteWithId);
+
   fs.writeFile("db.json", JSON.stringify(db), err => {
     if (err) {
       console.error(err);
       return res.status(500).send("Error adding that note");
     }
   });
-  res.status(201).json(newNote);
+
+  res.status(201).json(newNoteWithId);
 });
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
-
-// Method to generate unique ID
-//generateUniqueID() {
-// return crypto.randomBytes(8).toString('hex')
-// }
-//}
