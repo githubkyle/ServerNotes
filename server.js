25ad8bef05ad26cd5aa17e8f3172e646b3f194ed
kshwi <kshwiss@gmail.com>
2023-04-23 20:18:16 -0500

    commented out the erroring line

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/notes.html"))
 );
 
-app.get("/", (req, res) => res.sendFile(path.join(__dirname, "/index.html")));
+// app.get("/", (req, res) => res.sendFile(path.join(__dirname, "/index.html")));
 // app.get("*", (req, res) => res.sendFile(path.join(__dirname, "/index.html")));
 
 app.get("/api/notes", (req, res) => res.JSON(path.join(__dirname, "/db.json")));
